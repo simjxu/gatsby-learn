01a68c99eebd0fe45297bead1031b724067118a3
Simon Xu <simonxu@Simons-MacBook-Pro.local>
2018-11-03 21:05:32 -0700

    added about

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link, grapql } from 'gatsby'
+
+// This is only used for linking in between different sites in the directory
+import Layout from '../components/layout'
+
+const AboutPage = ({ data }) => {
+  return (
+    <Layout>
+      <div style={{ color: `teal` }}>
+        <p>Such wow. Very React.</p>
+      </div>
+    </Layout>
+  )
+}
+
+export default AboutPage
